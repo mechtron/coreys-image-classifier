2c23cdef2b26a630646a8798dc9db7312fe54057
Corey <mechtrondev@gmail.com>
2018-11-11 21:35:16 -0800

    Add image classification caching via Redis

--- api/classifier/services.py
+++ api/classifier/services.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
+import hashlib
+import json
 import os
 import uuid
 
@@ -11,7 +13,11 @@ from keras.applications import (
     imagenet_utils,
     mobilenet,
 )
+import redis
 from requests import get
+
+
+REDIS_CLIENT = redis.StrictRedis(host='redis', port=6379)
 
 
 def download_image(image_url):
@@ -44,14 +50,40 @@ def delete_temp_image(image_path):
     os.remove(image_path)
 
 
-def classify_image(image_url):
-    print("Classifying image from URL {}..".format(image_url))
-    start_time = datetime.datetime.now()
+def process_new_image(image_url):
     keras.backend.clear_session()
     image_path = download_image(image_url)
     processed_image = preprocess_image(image_path)
     prediction = make_prediction(processed_image)
     delete_temp_image(image_path)
+    return prediction
+
+
+def string_sha1(input_string):
+    return hashlib.sha1(input_string.encode('utf-8')).hexdigest()
+
+
+def get_prediction_cached(image_url):
+    image_url_hashed = string_sha1(image_url)
+    current_cached_value = REDIS_CLIENT.get(image_url_hashed)
+    if current_cached_value:
+        print("Found a cached result for {}..".format(image_url))
+        return json.loads(current_cached_value.decode('utf-8'))
+    else:
+        print('New image URL detected, processing new image..')
+        prediction = process_new_image(image_url)
+        REDIS_CLIENT.set(
+            image_url_hashed,
+            json.dumps(prediction),
+            ex=3600,
+        )
+        return prediction
+
+
+def classify_image(image_url):
+    print("Classifying image from URL {}..".format(image_url))
+    start_time = datetime.datetime.now()
+    prediction = get_prediction_cached(image_url)
     end_time = datetime.datetime.now()
     processing_time_seconds = (end_time-start_time).total_seconds()
     print("Time to process {url}: {seconds}s".format(
