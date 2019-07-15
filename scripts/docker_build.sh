bdf949821fb6d5f42e2c3e667fefbe2a0bfc5400
Corey <mechtrondev@gmail.com>
2019-07-14 22:23:01 -0700

    Add GitLab CI pipeline for building DockerHub images

---
+++ scripts/docker_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$COMMIT_TAG" == "" ]
+then
+   IMAGE_TAG="$CI_COMMIT_SHORT_SHA"
+else
+   IMAGE_TAG=$(echo $COMMIT_TAG | cut -d "_" -f2)
+fi
+echo "Docker image tag is $IMAGE_TAG"
+docker build --pull -t "$IMAGE_NAME:$IMAGE_TAG" .
+docker push "$IMAGE_NAME"
