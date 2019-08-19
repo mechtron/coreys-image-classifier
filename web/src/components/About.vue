04fd7fa7ee90c1cc1d8b94ae55282f93c7b8eb79
Corey <mechtrondev@gmail.com>
2019-08-18 23:53:01 -0700

    Update About page to tech stack diagram

--- web/src/components/About.vue
+++ web/src/components/About.vue
@@ -1,8 +1,41 @@
 <template lang="html">
   <div class="content-centered">
-    <h2>
-      About
-    </h2>
-    <img src="./../assets/logo.png" alt="vuejs logo">
+    <h2>Tech Stack</h2>
+    <div class="card">
+      <div class="card-header">Frontend</div>
+      <div class="card-body">
+        <img class="img-fluid" src="./../assets/bootstrap.png" alt="Bootstrap">
+        <img class="img-fluid" src="./../assets/vue.png" alt="VueJS">
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">Backend</div>
+      <div class="card-body">
+        <img class="img-fluid" src="./../assets/python.png" alt="Python">
+        <img class="img-fluid" src="./../assets/drf.png" alt="Django REST Framework">
+        <img class="img-fluid" src="./../assets/tensorflow.png" alt="TensorFlow">
+        <br>
+        <img class="img-fluid" src="./../assets/keras.png" alt="Keras">
+        <img class="img-fluid" src="./../assets/mysql.png" alt="MySQL">
+        <img class="img-fluid" src="./../assets/redis.png" alt="Redis">
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">DevOps</div>
+      <div class="card-body">
+        <img class="img-fluid" src="./../assets/docker.png" alt="Docker">
+        <img class="img-fluid" src="./../assets/k8s.png" alt="Kubernetes">
+        <img class="img-fluid" src="./../assets/eks.png" alt="EKS">
+        <img class="img-fluid" src="./../assets/helm.png" alt="Helm">
+        <img class="img-fluid" src="./../assets/ambassador.png" alt="Ambassador">
+        <img class="img-fluid" src="./../assets/envoy.png" alt="Envoy">
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+  .img-fluid {
+    height: 95px;
+  }
+</style>
